a6a08e16a3f6b8fcf7454e8e058e7a0aaec9b027
jay-galileo-academy <jay@galileo-academy.nl>
2022-10-06 15:28:48 +0200

    Update

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
 <?php wp_footer(); ?>
+    <div class="footer-wrapper">
+        <div class="footer-wrapper__inner">
+            <div class="footer-wrapper__inner--left">
+                <img src="<?php the_field('primary_logo', 'options') ?>" height="70" />
+            </div>
+            <div class="footer-wrapper__inner--right">
+                <p class="p-small"><?php the_field('bedrijfsgegevens', 'options', false) ?></p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
