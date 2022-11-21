4a97c18596c62ac2c8bc45fa43c76b16c8353b3b
jay-galileo-academy <jay@galileo-academy.nl>
2022-11-21 15:07:07 +0100

    Update
    - Attempts with Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 var concat = require('gulp-concat');
+const autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
 
 sass.compiler = require('node-sass');
 
@@ -10,6 +12,7 @@ gulp.task('css-compile', function (path) {
    return gulp.src('./assets/**/*.scss')
    .pipe(concat('app.scss'))
    .pipe(sass().on('error', sass.logError))
+   .pipe(postcss([autoprefixer() ]))
    .pipe(gulp.dest('./assets/css/'));
 });
 
